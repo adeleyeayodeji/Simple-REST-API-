77a29de4679d183e4517174aae64c145d3b4f4a6
Adeleye Ayodeji <42518062+adeleyeayodeji@users.noreply.github.com>
2020-03-12 06:39:10 -0700

    Update autoload.php

--- include/autoload.php
+++ include/autoload.php
@@ -1,4 +1,8 @@
 <?php
+    //Fix cross Origin
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
     spl_autoload_register("loadclass");
     
